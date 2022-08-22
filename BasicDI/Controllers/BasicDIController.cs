946b65e031077c64354be68b00b0f867e3c6057c
Peter Aryanto <peter.aryanto@gmail.com>
2022-08-22 23:09:43 +1000

    Added BasicDIController

---
+++ BasicDI/Controllers/BasicDIController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasicDI.Controllers
+{
+  [Route("api/[controller]")]
+  public class BasicDIController : Controller
+  {
+    /*
+    public IActionResult Index()
+    {
+      return View();
+    }
+    */
+
+    public IActionResult Get()
+    {
+      return Ok(
+        new
+        {
+          Result = "Hello World!",
+        }
+      );
+    }
+  }
+}
